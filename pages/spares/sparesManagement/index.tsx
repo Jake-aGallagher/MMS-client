e69794f6cf71665ae0300d579f8ecc44ea7839ac
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-04-13 21:08:43 +0100

    Update delivery info tab

--- pages/spares/sparesManagement/index.tsx
+++ pages/spares/sparesManagement/index.tsx
@@ -21,7 +21,7 @@ const SparesManagement = () => {
                     <div className="px-10 pt-5">
                         <div className="bg-white p-6 rounded-lg shadow-lg">
                             <h2 className="text-xl font-semibold mb-2">Deliveries</h2>
-                            <p className="">Manage Deliveries info here</p>
+                            <p className="">Add deliveries, manage scheduled deliveries and confirm when deliveries have arrived to automatically update current stock levels with the newly arrived items</p>
                             <button className="rounded-3xl bg-blue-50 hover:bg-blue-600 h-8 px-4 mt-4 border-2 border-blue-600 hover:border-transparent">
                                 <Link href="/spares/sparesManagement/deliveries">Manage Deliveries</Link>
                             </button>
