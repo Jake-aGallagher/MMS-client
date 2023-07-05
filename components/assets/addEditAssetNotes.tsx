eecd97917ec3e3bf4f83a3c64bffcebd9832e19d
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-07-05 20:50:06 +0100

    Convert AssetNotes

--- components/assets/addEditAssetNotes.tsx
+++ components/assets/addEditAssetNotes.tsx
@@ -1,6 +1,11 @@
-import axios from "axios";
-import { useState } from "react";
-import { SERVER_URL } from "../routing/addressAPI";
+import axios from 'axios';
+import { SERVER_URL } from '../routing/addressAPI';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import FormHeader from '../forms/formHeader';
+import GeneralFormSubmit from '../forms/generalFormSubmit';
+import GeneralFormInput from '../forms/generalFormInput';
 
 interface ModalProps {
     closeModal: () => void;
@@ -8,52 +13,51 @@ interface ModalProps {
 }
 
 const AddEditAssetNotes = (props: ModalProps) => {
-    const [note, setNote] = useState(props.payload.note)
+    const alertString = `There has been an issue ${props.payload.id > 0 ? 'editing' : 'creating'} this Note, please try again.`;
+    const defaultValues = { note: props.payload.note };
 
-    const submitHandler = async (e: React.MouseEvent<HTMLElement>) => {
-        e.preventDefault();
+    const formValidation = yup.object().shape({
+        note: yup.string().max(1000),
+    });
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(formValidation),
+        defaultValues: defaultValues,
+    });
+
+    const handleRegistration = async (data: any) => {
         try {
             const response = await axios.put(
                 `${SERVER_URL}/asset/notes`,
                 {
                     id: props.payload.id,
-                    note
+                    note: data.note,
                 },
                 { headers: { Authorisation: 'Bearer ' + localStorage.getItem('token') } }
             );
             if (response.data.created) {
                 props.closeModal();
             } else {
-                {
-                    props.payload.id > 0
-                        ? alert('There has been an issue editing this Note, please try again.')
-                        : alert('There has been an issue creating this Note, please try again.');
-                }
+                alert(alertString);
             }
         } catch (err) {
-            {
-                props.payload.id > 0
-                    ? alert('There has been an issue editing this Note, please try again.')
-                    : alert('There has been an issue creating this Note, please try again.');
-            }
+            alert(alertString);
         }
     };
 
     return (
         <div className="h-full w-full rounded-lg relative border-4 border-blue-200">
-    <h1 className="w-full h-10 flex flex-row justify-center items-center font-bold bg-blue-200">{props.payload.id > 0 ? 'Edit Notes' : 'Add Note'}</h1>
-    <form className="flex flex-col justify-start px-4 pt-2 overflow-y-auto h-[calc(100%-104px)]">
-
-        <label htmlFor="note">Notes:</label>
-        <textarea id="note" rows={10} className="mb-2 rounded-sm bg-blue-200" value={note} onChange={(e) => setNote(e.target.value)} />
-
-        <div className="flex flex-row justify-evenly items-center absolute bottom-0 h-16 left-0 w-full bg-blue-200">
-            <button className="rounded-3xl bg-blue-50 hover:bg-blue-600 h-8 px-4  border-2 border-blue-600 w-32" onClick={props.closeModal}>Cancel</button>
-            <button className="rounded-3xl bg-blue-50 hover:bg-blue-600 h-8 px-4  border-2 border-blue-600 w-32" onClick={submitHandler}>Submit</button>
+            <FormHeader label={props.payload.id > 0 ? 'Edit Notes' : 'Add Note'} />
+            <form onSubmit={handleSubmit(handleRegistration)} className="flex flex-col justify-start px-4 pt-2 overflow-y-auto h-[calc(100%-104px)]">
+                <GeneralFormInput register={register} label="Notes" type="textarea" formName="note" errors={errors} rows={10} />
+                <GeneralFormSubmit closeModal={props.closeModal} />
+            </form>
         </div>
-    </form>
-</div>
-    )
-}
+    );
+};
 
-export default AddEditAssetNotes
+export default AddEditAssetNotes;
