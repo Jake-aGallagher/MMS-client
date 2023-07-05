c29b32c2f381f92a4799bcd08ac075c0c0022735
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-07-05 21:19:21 +0100

    Convert sparesNote to RHF

--- components/spares/sparesManagement/addEditSparesNote.tsx
+++ components/spares/sparesManagement/addEditSparesNote.tsx
@@ -1,10 +1,16 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import RetrieveError from '../../error/retrieveError';
 import Loading from '../../loading/loading';
 import { RootState } from '../../store/store';
 import { SERVER_URL } from '../../routing/addressAPI';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import FormHeader from '../../forms/formHeader';
+import GeneralFormSubmit from '../../forms/generalFormSubmit';
+import GeneralFormInput from '../../forms/generalFormInput';
 
 interface ModalProps {
     closeModal: () => void;
@@ -14,11 +20,33 @@ interface ModalProps {
 const AddEditSparesNote = (props: ModalProps) => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
+    const alertString = `There has been an issue ${props.payload.id > 0 ? 'editing' : 'creating'} this Note, please try again.`;
     const currentProperty = useSelector((state: RootState) => state.currentProperty.value.currentProperty);
     const [title, setTitle] = useState('');
     const [note, setNote] = useState('');
     const [count, setcount] = useState(0);
     const noteId = props.payload.id;
+    const [defaultValues, setDefaultValues] = useState({title: '', note: ''})
+
+    const formValidation = yup.object().shape({
+        title: yup.string().required().max(45),
+        note: yup.string().required().max(1000),
+    });
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        watch,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(formValidation),
+        defaultValues: useMemo(() => {
+            return defaultValues;
+        }, [defaultValues]),
+    });
+
+    const watchType = watch(['note']);
 
     useEffect(() => {
         if (props.payload.id > 0) {
@@ -30,13 +58,16 @@ const AddEditSparesNote = (props: ModalProps) => {
         }
     }, []);
 
+    useEffect(() => {
+        reset(defaultValues);
+    }, [defaultValues]);
+
     const getHandler = async () => {
         try {
             const response = await axios.get(`${SERVER_URL}/spares/note/${props.payload?.id}`, {
                 headers: { Authorisation: 'Bearer ' + localStorage.getItem('token') },
             });
-            setTitle(response.data[0].title);
-            setNote(response.data[0].content);
+            setDefaultValues({title: response.data[0].title, note: response.data[0].content});
             setcount(response.data[0].content.length);
             setLoading(false);
         } catch (err) {
@@ -45,38 +76,27 @@ const AddEditSparesNote = (props: ModalProps) => {
         }
     };
 
-    const submitHandler = async (e: React.MouseEvent<HTMLElement>) => {
-        e.preventDefault();
-        if (title.length > 0 && note.length > 0) {
-            try {
-                const response = await axios.put(
-                    `${SERVER_URL}/spares/notes`,
-                    {
-                        propertyId: currentProperty,
-                        title,
-                        note,
-                        noteId,
-                    },
-                    {
-                        headers: { Authorisation: 'Bearer ' + localStorage.getItem('token') },
-                    }
-                );
-                if (response.data.created) {
-                    props.closeModal();
-                } else {
-                    {
-                        props.payload.id > 0
-                            ? alert('There has been an issue editing this Note, please try again.')
-                            : alert('There has been an issue creating this Note, please try again.');
-                    }
-                }
-            } catch (err) {
+    const handleRegistration = async (data: any) => {
+        try {
+            const response = await axios.put(
+                `${SERVER_URL}/spares/notes`,
                 {
-                    props.payload.id > 0
-                        ? alert('There has been an issue editing this Note, please try again.')
-                        : alert('There has been an issue creating this Note, please try again.');
+                    propertyId: currentProperty,
+                    title: data.title,
+                    note: data.note,
+                    noteId: props.payload.id,
+                },
+                {
+                    headers: { Authorisation: 'Bearer ' + localStorage.getItem('token') },
                 }
+            );
+            if (response.data.created) {
+                props.closeModal();
+            } else {
+                alert(alertString);
             }
+        } catch (err) {
+            alert(alertString);
         }
     };
 
@@ -88,38 +108,13 @@ const AddEditSparesNote = (props: ModalProps) => {
                 <RetrieveError />
             ) : (
                 <div className="h-full w-full rounded-lg relative border-4 border-blue-200">
-                    <h1 className="w-full h-10 flex flex-row justify-center items-center font-bold bg-blue-200">
-                        {props.payload.title.length > 0 ? props.payload.title : 'Add Note'}
-                    </h1>
-                    <form className="flex flex-col justify-start px-4 pt-2 overflow-y-auto h-[calc(100%-104px)]">
+                    <FormHeader label={props.payload.title.length > 0 ? props.payload.title : 'Add Note'} />
+                    <form onSubmit={handleSubmit(handleRegistration)} className="flex flex-col justify-start px-4 pt-2 overflow-y-auto h-[calc(100%-104px)]">
                         <div className="text-center">This note will be visible to anyone who visits the Spares Management Page</div>
-
-                        <label htmlFor="title">Title:</label>
-                        <input id="title" type="text" maxLength={45} className="mb-2 rounded-sm bg-blue-200" value={title} onChange={(e) => setTitle(e.target.value)} />
-
-                        <label htmlFor="note">Note:</label>
-                        <textarea
-                            id="note"
-                            className="my-2 rounded-sm bg-blue-200"
-                            rows={12}
-                            maxLength={1000}
-                            value={note}
-                            onChange={(e) => [setNote(e.target.value), setcount(e.target.value.length)]}
-                        />
-
-                        <div className="text-center">{count} / 1000 Charachters</div>
-
-                        <div className="flex flex-row justify-evenly items-center absolute bottom-0 h-16 left-0 w-full bg-blue-200">
-                            <button className="rounded-3xl bg-blue-50 hover:bg-blue-600 h-8 px-4  border-2 border-blue-600 w-32" onClick={props.closeModal}>
-                                Cancel
-                            </button>
-                            <button
-                                className="rounded-3xl bg-blue-50 hover:bg-blue-600 h-8 px-4  border-2 border-blue-600 w-32"
-                                onClick={(e) => submitHandler(e)}
-                            >
-                                Submit
-                            </button>
-                        </div>
+                        <GeneralFormInput register={register} label="Title" type="text" formName="title" errors={errors} required={true} />
+                        <GeneralFormInput register={register} label="Note" type="textarea" formName="note" errors={errors} rows={10} />
+                        <div className="text-center">{watchType[0].length} / 1000 Charachters</div>
+                        <GeneralFormSubmit closeModal={props.closeModal} />
                     </form>
                 </div>
             )}
