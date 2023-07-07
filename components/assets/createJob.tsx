b204e277354758dc77efa725e1b758a5246ea2d2
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-07-07 22:42:39 +0100

    Convert createJob

--- components/assets/createJob.tsx
+++ components/assets/createJob.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import axios from 'axios';
@@ -6,18 +6,18 @@ import Loading from '../loading/loading';
 import RetrieveError from '../error/retrieveError';
 import ModalBase from '../modal/modal';
 import { SERVER_URL } from '../routing/addressAPI';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import FormHeader from '../forms/formHeader';
+import GeneralFormSubmit from '../forms/generalFormSubmit';
+import GeneralFormInput from '../forms/generalFormInput';
+import FormContainer from '../forms/formContainer';
+import GeneralForm from '../forms/generalForm';
 
 interface ModalProps {
     closeModal: () => void;
     assetId: number;
-}
-
-interface State {
-    assetNumber: string;
-    title: string;
-    description: string;
-    reqCompDate: Date;
-    compNow: string;
 }
 
 interface TypeOptions {
@@ -37,16 +37,41 @@ const CreateJob = (props: ModalProps) => {
     const userId = useSelector((state: RootState) => state.user.value.id);
     const currentProperty = useSelector((state: RootState) => state.currentProperty.value.currentProperty);
     const [typeOptions, setTypeOptions] = useState<TypeOptions[]>([]);
-    const [selectedType, setSelectedType] = useState('');
     const [urgencyOptions, setUrgencyOptions] = useState<UrgencyOptions[]>([]);
-    const [selectedUrgency, setSelectedUrgency] = useState('');
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [reqCompDate, setReqCompDate] = useState('0000-00-00 00:00:00');
-    const [compNow, setCompNow] = useState('No');
     const [viewModal, setViewModal] = useState(false);
     const [modalType, setModalType] = useState('');
     const [modalPayload, setModalPayload] = useState(0);
+    const compNowOptions = [
+        { id: 'No', value: 'No' },
+        { id: 'Yes', value: 'Yes' },
+    ];
+    const [defaultValues, setDefaultValues] = useState({
+        selectedType: '',
+        title: '',
+        description: '',
+        selectedUrgency: '',
+        compNow: 'No',
+    });
+
+    const formValidation = yup.object().shape({
+        selectedType: yup.string().required(),
+        title: yup.string().required().max(255),
+        description: yup.string().required().max(1000),
+        selectedUrgency: yup.string().required(),
+        compNow: yup.string().required(),
+    });
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(formValidation),
+        defaultValues: useMemo(() => {
+            return defaultValues;
+        }, [defaultValues]),
+    });
 
     useEffect(() => {
         setLoading(true);
@@ -55,15 +80,22 @@ const CreateJob = (props: ModalProps) => {
         getEnums();
     }, []);
 
+    useEffect(() => {
+        reset(defaultValues);
+    }, [defaultValues]);
+
     const getEnums = async () => {
         try {
             const response = await axios.get(`${SERVER_URL}/enums/create-job`, {
                 headers: { Authorisation: 'Bearer ' + localStorage.getItem('token') },
             });
             setTypeOptions(response.data.types);
-            setSelectedType(response.data.types[0].id);
             setUrgencyOptions(response.data.urgency);
-            setSelectedUrgency(response.data.urgency[0].id);
+            setDefaultValues({
+                ...defaultValues,
+                selectedType: response.data.types[0].id,
+                selectedUrgency: response.data.urgency[0].id,
+            });
             setLoading(false);
         } catch (err) {
             setError(true);
@@ -71,28 +103,26 @@ const CreateJob = (props: ModalProps) => {
         }
     };
 
-    const submitHandler = async (e: React.MouseEvent<HTMLElement>) => {
-        e.preventDefault();
+    const handleRegistration = async (data: any) => {
         try {
             const response = await axios.post(
                 `${SERVER_URL}/jobs`,
                 {
                     propertyNumber: currentProperty,
                     assetNumber: props.assetId,
-                    type: selectedType,
-                    title: title,
-                    description: description,
-                    urgency: selectedUrgency,
-                    reqCompDate: reqCompDate,
+                    type: data.selectedType,
+                    title: data.title,
+                    description: data.description,
+                    urgency: data.selectedUrgency,
                     reporter: userId,
                 },
                 {
                     headers: { Authorisation: 'Bearer ' + localStorage.getItem('token') },
                 }
             );
-            if (response.data.created && compNow == 'No') {
+            if (response.data.created && data.compNow == 'No') {
                 props.closeModal();
-            } else if (response.data.created && compNow == 'Yes') {
+            } else if (response.data.created && data.compNow == 'Yes') {
                 setModalPayload(response.data.jobId);
                 setModalType('updateJob');
                 setViewModal(true);
@@ -113,65 +143,44 @@ const CreateJob = (props: ModalProps) => {
             ) : (
                 <>
                     {viewModal ? <ModalBase modalType={modalType} payload={modalPayload} fullSize={true} closeModal={() => [setViewModal(false), props.closeModal()]} /> : ''}
-                    <div className="h-full w-full rounded-lg relative border-4 border-blue-200">
-                        <h1 className="w-full h-10 flex flex-row justify-center items-center font-bold bg-blue-200">Create New Job</h1>
-                        <form className="flex flex-col justify-start px-4 pt-2 overflow-y-auto h-[calc(100%-104px)]">
-                            <label htmlFor="type">Job Type:</label>
-                            <select id="type" className="mb-2 rounded-sm bg-blue-200" onChange={(e) => setSelectedType(e.target.value)}>
-                                {typeOptions.map((typeOption) => (
-                                    <option value={typeOption.id} key={'typeOption_' + typeOption.id}>
-                                        {typeOption.value}
-                                    </option>
-                                ))}
-                            </select>
-
-                            <label htmlFor="title">Title:</label>
-                            <input id="title" type="text" maxLength={255} className="mb-2 rounded-sm bg-blue-200" onChange={(e) => setTitle(e.target.value)} />
-
-                            <label htmlFor="description">Job Description:</label>
-                            <textarea
-                                id="description"
-                                rows={5}
-                                className="mb-2 rounded-sm bg-blue-200 resize-none"
-                                onChange={(e) => setDescription(e.target.value)}
+                    <FormContainer>
+                        <FormHeader label={'Create New Job'} />
+                        <GeneralForm handleSubmit={handleSubmit} handleRegistration={handleRegistration}>
+                            <GeneralFormInput
+                                register={register}
+                                label="Job Type"
+                                type="select"
+                                formName="selectedType"
+                                errors={errors}
+                                required={true}
+                                optionNameString="value"
+                                selectOptions={typeOptions}
                             />
-
-                            <label htmlFor="type">Urgency:</label>
-                            <select id="type" className="mb-2 rounded-sm bg-blue-200" onChange={(e) => setSelectedUrgency(e.target.value)}>
-                                {urgencyOptions.map((urgencyOption) => (
-                                    <option value={urgencyOption.id} key={'urgencyOption_' + urgencyOption.value}>
-                                        {urgencyOption.value}
-                                    </option>
-                                ))}
-                            </select>
-
-                            <div>Would you like to update and/or complete this Job immediately?</div>
-                            <div className="flex flex-row justify-start items-center pl-2 gap-3 py-1">
-                                <label htmlFor="no">No</label>
-                                <input type="radio" id="no" name="comp_immediately" value="No" checked={compNow == 'No'} onChange={() => setCompNow('No')} />
-                            </div>
-                            <div className="flex flex-row justify-start items-center pl-2 gap-3 py-1">
-                                <label htmlFor="yes">Yes</label>
-                                <input
-                                    type="radio"
-                                    id="yes"
-                                    name="comp_immediately"
-                                    value="Yes"
-                                    checked={compNow == 'Yes'}
-                                    onChange={() => setCompNow('Yes')}
-                                />
-                            </div>
-
-                            <div className="flex flex-row justify-evenly items-center absolute bottom-0 h-16 left-0 w-full bg-blue-200">
-                                <button className="rounded-3xl bg-blue-50 hover:bg-blue-600 h-8 px-4  border-2 border-blue-600 w-32" onClick={props.closeModal}>
-                                    Cancel
-                                </button>
-                                <button className="rounded-3xl bg-blue-50 hover:bg-blue-600 h-8 px-4  border-2 border-blue-600 w-32" onClick={submitHandler}>
-                                    Submit
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+                            <GeneralFormInput register={register} label="Title" type="text" formName="title" errors={errors} required={true} />
+                            <GeneralFormInput register={register} label="Job Description" type="textarea" formName="description" errors={errors} rows={5} />
+                            <GeneralFormInput
+                                register={register}
+                                label="Urgency"
+                                type="select"
+                                formName="selectedUrgency"
+                                errors={errors}
+                                required={true}
+                                optionNameString="value"
+                                selectOptions={urgencyOptions}
+                            />
+                            <GeneralFormInput
+                                register={register}
+                                label="Would you like to update and/or complete this Job immediately"
+                                type="select"
+                                formName="compNow"
+                                errors={errors}
+                                required={true}
+                                optionNameString="value"
+                                selectOptions={compNowOptions}
+                            />
+                            <GeneralFormSubmit closeModal={props.closeModal} />
+                        </GeneralForm>
+                    </FormContainer>
                 </>
             )}
             ;
