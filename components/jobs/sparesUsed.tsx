9f6050b67e7be72d34f014b244e3e53bc3c2b332
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-07-11 17:51:31 +0100

    Initial refactoring of spares used wizard

--- components/jobs/sparesUsed.tsx
+++ components/jobs/sparesUsed.tsx
@@ -4,10 +4,12 @@ import GreaterThan from '../../public/GreaterThan.png';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import { SERVER_URL } from '../routing/addressAPI';
+import SparesSearch from '../sparesForm/sparesSearch';
+import SortableTable from '../sortableTable/sortableTable';
 
 interface ModalProps {
     closeModal: () => void;
-    payload: { sparesUsed: SparesUsed[], type: string };
+    payload: { sparesUsed: SparesUsed[]; type: string };
     passbackDetails: (usedSparesArray: Spare[]) => void;
 }
 
@@ -21,30 +23,36 @@ interface SparesUsed extends Spare {
     num_used: number;
 }
 
+const usedConfig = {
+    headers: [
+        { id: 'Part Number', name: 'Part Number', type: 'string', nameParam: 'part_no', search: false, order: false },
+        { id: 'name', name: 'Name', type: 'string', search: false, order: false },
+        { id: 'num_used', name: 'Quantity', type: 'numver', search: false, order: false },
+    ],
+    searchable: false,
+};
+
 const SparesUsed = (props: ModalProps) => {
     const [loading, setLoading] = useState(true);
-    const [noData, setNoData] = useState(false);
     const [error, setError] = useState(false);
     const currentProperty = useSelector((state: RootState) => state.currentProperty.value.currentProperty);
     const [sparesFullList, setSparesFullList] = useState<Spare[]>([]);
     const [sparesFiltered, setSparesFiltered] = useState<SparesUsed[]>([]);
     const [sparesUsed, setSparesUsed] = useState<SparesUsed[]>([]);
-    const [chosenFilter, setChosenFilter] = useState(0);
-    const [searchTerm, setSearchTerm] = useState('');
+    const [searchFilter, setSearchFilter] = useState({ type: 0, value: '' });
     const [showRes, setShowRes] = useState(false);
     const [numResults, setNumResults] = useState(0);
 
     useEffect(() => {
         setLoading(true);
         setError(false);
-        setNoData(false);
         setSparesUsed(props.payload.sparesUsed);
         getHandler();
     }, []);
 
     useEffect(() => {
         filterResults();
-    }, [sparesFullList, chosenFilter, searchTerm]);
+    }, [sparesFullList, searchFilter]);
 
     const getHandler = async () => {
         try {
@@ -53,7 +61,6 @@ const SparesUsed = (props: ModalProps) => {
             });
             setNumResults(response.data.spares.length);
             if (response.data.spares.length === 0) {
-                setNoData(true);
             } else {
                 setSparesFullList(response.data.spares);
             }
@@ -66,19 +73,19 @@ const SparesUsed = (props: ModalProps) => {
 
     const filterResults = () => {
         let fList: SparesUsed[] = [];
-        if (searchTerm.length === 0) {
+        if (searchFilter.value.length === 0) {
             sparesFullList.forEach((spare) => {
                 fList.push({ ...spare, num_used: 0 });
             });
-        } else if (chosenFilter === 0) {
+        } else if (searchFilter.type === 0) {
             sparesFullList.forEach((spare) => {
-                if (spare.part_no.toLowerCase().includes(searchTerm.toLowerCase())) {
+                if (spare.part_no.toLowerCase().includes(searchFilter.value.toLowerCase())) {
                     fList.push({ ...spare, num_used: 0 });
                 }
             });
         } else {
             sparesFullList.forEach((spare) => {
-                if (spare.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                if (spare.name.toLowerCase().includes(searchFilter.value.toLowerCase())) {
                     fList.push({ ...spare, num_used: 0 });
                 }
             });
@@ -117,10 +124,7 @@ const SparesUsed = (props: ModalProps) => {
                 },
             ]);
         } else {
-            setSparesUsed((prev) => [
-                ...prev,
-                { id: spareToUpdate.id, part_no: spareToUpdate.part_no, name: spareToUpdate.name, num_used: spareToUpdate.num_used },
-            ]);
+            setSparesUsed((prev) => [...prev, { id: spareToUpdate.id, part_no: spareToUpdate.part_no, name: spareToUpdate.name, num_used: spareToUpdate.num_used }]);
         }
         const filteredFilterList = sparesFiltered.filter((item) => item.id != spareToUpdate.id);
         const filterListUnordered = [...filteredFilterList, { id: spareToUpdate.id, part_no: spareToUpdate.part_no, name: spareToUpdate.name, num_used: 0 }];
@@ -142,7 +146,9 @@ const SparesUsed = (props: ModalProps) => {
             <div key={i.id} className={`flex flex-row border-2 border-blue-600 rounded-md mb-2 w-fit px-2 ${i.num_used < 1 ? 'hidden' : ''}`}>
                 <div className="mr-4">{i.part_no}</div>
                 <div className="mr-4">{i.name}</div>
-                <div className="mr-4">{props.payload.type === 'delivery' ? 'Quantity Ordered:' : 'Quantity Used:'} {i.num_used}</div>
+                <div className="mr-4">
+                    {props.payload.type === 'delivery' ? 'Quantity Ordered:' : 'Quantity Used:'} {i.num_used}
+                </div>
                 <button
                     onClick={(e) => {
                         removeUsedHandler(e, i.id);
@@ -175,52 +181,23 @@ const SparesUsed = (props: ModalProps) => {
         <div className="h-full w-full rounded-lg relative border-4 border-blue-200">
             <h1 className="w-full h-10 flex flex-row justify-center items-center font-bold bg-blue-200">{props.payload.type === 'delivery' ? 'Add to Delivery' : 'Spares Used'}</h1>
             <form className="flex flex-col justify-start px-4 pt-2 overflow-y-auto h-[calc(100%-104px)]">
-                <label htmlFor="searchInput" className="mb-2">
-                    Spares Search
-                </label>
-                <div className="flex flex-row mb-5">
-                    <select
-                        id="selectBy"
-                        className="rounded-sm bg-blue-200 mr-2"
-                        value={chosenFilter}
-                        onChange={(e) => setChosenFilter(parseInt(e.target.value))}
-                    >
-                        <option value={0}>Part Number / ID</option>
-                        <option value={1}>Part Name</option>
-                    </select>
-                    <input
-                        id="searchInput"
-                        type="text"
-                        className="rounded-sm bg-blue-200 w-full"
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                    />
-                    <div
-                        className="ml-2 rounded-3xl bg-blue-50 hover:bg-blue-600 h-8 px-4 border-2 border-blue-600 hover:border-transparent"
-                        onClick={(e) => [e.preventDefault(), setSearchTerm('')]}
-                    >
-                        Clear
-                    </div>
-                </div>
+                {sparesUsed.length > 0 ? <SortableTable data={sparesUsed} config={usedConfig} /> : null}
+
+                <SparesSearch searchFilter={searchFilter} setSearchFilter={setSearchFilter} />
+
                 <div>{props.payload.type === 'delivery' ? 'Items Ordered:' : 'Spares Used:'}</div>
                 <div className="mb-5 mt-1">{showSparesUsed}</div>
 
                 <div className="mb-5">
                     <div>Search Results</div>
-                    <div
-                        className="flex flex-row items-center hover:text-blue-600 icon-filter hover:cursor-pointer select-none"
-                        onClick={() => setShowRes(!showRes)}
-                    >
+                    <div className="flex flex-row items-center hover:text-blue-600 icon-filter hover:cursor-pointer select-none" onClick={() => setShowRes(!showRes)}>
                         <img className={`mr-1 h-5 w-5 font-bold text-2xl duration-150 ${showRes ? 'rotate-90' : null}`} src={GreaterThan.src} />
                         <div>{numResults} Results</div>
                     </div>
                     {showRes ? <div>{showSparesFiltered}</div> : null}
                 </div>
                 <div className="flex flex-row justify-evenly items-center absolute bottom-0 h-16 left-0 w-full bg-blue-200">
-                    <button
-                        className="rounded-3xl bg-blue-50 hover:bg-blue-600 h-8 px-4  border-2 border-blue-600 w-32"
-                        onClick={(e) => [e.preventDefault(), props.closeModal()]}
-                    >
+                    <button className="rounded-3xl bg-blue-50 hover:bg-blue-600 h-8 px-4  border-2 border-blue-600 w-32" onClick={(e) => [e.preventDefault(), props.closeModal()]}>
                         Cancel
                     </button>
                     <button className="rounded-3xl bg-blue-50 hover:bg-blue-600 h-8 px-4  border-2 border-blue-600 w-32" onClick={(e) => SubmitHandler(e)}>
---
+++ components/sparesForm/sparesSearch.tsx
@@ -0,0 +1,47 @@
+import { Dispatch, SetStateAction } from 'react';
+
+interface Props {
+    searchFilter: { type: number; value: string };
+    setSearchFilter: Dispatch<
+        SetStateAction<{
+            type: number;
+            value: string;
+        }>
+    >;
+}
+
+const SparesSearch = (props: Props) => {
+    return (
+        <>
+            <label htmlFor="searchInput" className="mb-2">
+                Spares Search
+            </label>
+            <div className="flex flex-row mb-5">
+                <select
+                    id="selectBy"
+                    className="rounded-sm bg-blue-200 mr-2"
+                    value={props.searchFilter.type}
+                    onChange={(e) => props.setSearchFilter({ type: parseInt(e.target.value), value: props.searchFilter.value })}
+                >
+                    <option value={0}>Part Number / ID</option>
+                    <option value={1}>Part Name</option>
+                </select>
+                <input
+                    id="searchInput"
+                    type="text"
+                    className="rounded-sm bg-blue-200 w-full"
+                    value={props.searchFilter.value}
+                    onChange={(e) => props.setSearchFilter({ type: props.searchFilter.type, value: e.target.value })}
+                />
+                <div
+                    className="ml-2 rounded-3xl bg-blue-50 hover:bg-blue-600 h-8 px-4 border-2 border-blue-600 hover:border-transparent"
+                    onClick={(e) => [e.preventDefault(), props.setSearchFilter({ type: props.searchFilter.type, value: '' })]}
+                >
+                    Clear
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default SparesSearch;
