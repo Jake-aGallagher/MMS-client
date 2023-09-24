cfdc5baac6ff348b2ccccb9d121ba2584cae24b8
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-09-24 13:08:45 +0100

    fix route call

--- components/jobs/updateJob/updateJobFullConn.ts
+++ components/jobs/updateJob/updateJobFullConn.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 import { SERVER_URL } from '../../routing/addressAPI';
 
 export const updateJobFullConn = async (data: FormData) => {
-    return await axios.put(`${SERVER_URL}/jobs/notes`, data, { headers: { Authorisation: 'Bearer ' + localStorage.getItem('token') } });
+    return await axios.put(`${SERVER_URL}/jobs/update`, data, { headers: { Authorisation: 'Bearer ' + localStorage.getItem('token') } });
 };
