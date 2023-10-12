7d348781770b6b46419ca7317632654e46c36b7b
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-10-12 23:43:24 +0100

    Form input Styling

--- components/forms/generalFormInput.tsx
+++ components/forms/generalFormInput.tsx
@@ -26,45 +26,61 @@ const GeneralFormInput = (props: Props<any>) => {
 
     return (
         <>
-            {props.label && props.type != 'checkbox' ? <label htmlFor={props.formName} className='w-full text-left'>{props.label}: </label> : null}
-            {singleTypes.includes(props.type) && (
-                <input
-                    id={props.formName}
-                    type={props.type}
-                    min={props.min}
-                    className={`mb-2 rounded-sm bg-secondary ${props.extraClasses && props.extraClasses} ${props.errors[props.formName] && 'border-red border-2'}`}
-                    {...props.register(props.formName, { required: props.required, valueAsNumber: props.type == 'number' })}
-                />
-            )}
-            {areaTypes.includes(props.type) && (
-                <textarea
-                    id={props.formName}
-                    rows={props.rows}
-                    className={`mb-2 rounded-sm bg-secondary ${props.extraClasses && props.extraClasses} ${props.errors[props.formName] && 'border-red border-2'}`}
-                    {...props.register(props.formName, { required: props.required })}
-                />
-            )}
-            {selectTypes.includes(props.type) && (
-                <select
-                    id={props.formName}
-                    className={`mb-2 rounded-sm bg-secondary ${props.extraClasses && props.extraClasses} ${props.errors[props.formName] && 'border-red border-2'}`}
-                    {...props.register(props.formName, { required: props.required })}
-                >
-                    {props.selectOptions!.map((item) => (
-                        <option value={item.id} key={props.formName + '_' + item.id}>
-                            {item[props.optionNameString!]}
-                        </option>
-                    ))}
-                </select>
-            )}
-            {checkboxTypes.includes(props.type) && (
-                <div className={`rounded-md my-2 p-2 border-2 bg-secondary border-primary w-full flex flex-row ${props.extraClasses && props.extraClasses} ${props.errors[props.formName] && 'border-red border-2'}`}>
-                    <label htmlFor={props.formName} className="w-full">
-                        {props.label}
+            <div className="flex flex-col mx-1 relative mb-2">
+
+                {props.label && props.type != 'checkbox' ? (
+                    <label htmlFor={props.formName} className='text-sm absolute ml-3 px-1 -top-1 z-10 bg-background'>
+                        {props.label}:{' '}
                     </label>
-                    <input id={props.formName} type="checkbox" className="mx-2" {...props.register(props.formName, { required: props.required, value: props.checked})} />
-                </div>
-            )}
+                ) : null}
+
+                {singleTypes.includes(props.type) && (
+                    <input
+                        id={props.formName}
+                        type={props.type}
+                        min={props.min}
+                        className={`h-10 pl-1 my-2 rounded-md bg-background w-full border-1 border-primary border-solid ${props.extraClasses && props.extraClasses} ${props.errors[props.formName] && 'border-red border-2'}`}
+                        {...props.register(props.formName, { required: props.required, valueAsNumber: props.type == 'number' })}
+                    />
+                )}
+
+                {areaTypes.includes(props.type) && (
+                    <textarea
+                        id={props.formName}
+                        rows={props.rows}
+                        className={`h-20 pl-1 pt-2 my-2 rounded-md bg-background w-full border-1 border-primary border-solid ${props.extraClasses && props.extraClasses} ${props.errors[props.formName] && 'border-red border-2'}`}
+                        {...props.register(props.formName, { required: props.required })}
+                    />
+                )}
+
+                {selectTypes.includes(props.type) && (
+                    <select
+                        id={props.formName}
+                        className={`h-10 pl-1 my-2 rounded-md bg-background w-full border-1 border-primary border-solid ${props.extraClasses && props.extraClasses} ${props.errors[props.formName] && 'border-red border-2'}`}
+                        {...props.register(props.formName, { required: props.required })}
+                    >
+                        {props.selectOptions!.map((item) => (
+                            <option value={item.id} key={props.formName + '_' + item.id}>
+                                {item[props.optionNameString!]}
+                            </option>
+                        ))}
+                    </select>
+                )}
+
+                {checkboxTypes.includes(props.type) && (
+                    <div
+                        className={`rounded-md my-2 p-2 border-2 bg-secondary border-primary w-full flex flex-row ${props.extraClasses && props.extraClasses} ${
+                            props.errors[props.formName] && 'border-red border-2'
+                        }`}
+                    >
+                        <label htmlFor={props.formName} className="w-full">
+                            {props.label}
+                        </label>
+                        <input id={props.formName} type="checkbox" className="mx-2" {...props.register(props.formName, { required: props.required, value: props.checked })} />
+                    </div>
+                )}
+
+            </div>
         </>
     );
 };
