2bd6e24558c2db1dc4c8d4cfbfa20934b46a96e3
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-10-15 22:54:08 +0100

    Change detailsBox Styles into card

--- components/detailsBox/detailsBox.tsx
+++ components/detailsBox/detailsBox.tsx
@@ -10,9 +10,9 @@ interface Props {
 
 const DetailsBox = (props: Props) => {
     return (
-        <div className="ml-10 mb-4  max-w-3xl">
+        <div className=" mb-4 w-full max-w-3xl bg-secondary mt-2 p-2 rounded-xl shadow-xl">
             {props.data.fields.map((item) => (
-                <div key={item.label} className="flex flex-row mb-3 pb-1 border-b-1 border-secondary">
+                <div key={item.label} className="flex flex-row mb-3 pb-1 border-b-1 border-text border-opacity-10">
                     <div className="w-1/4 pl-1">{item.label}: </div>
                     <div className="w-3/4 pl-4">{item.value}</div>
                 </div>
