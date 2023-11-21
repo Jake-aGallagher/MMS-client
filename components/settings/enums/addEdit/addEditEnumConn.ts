b509e678eba2373f17faa3340adf416dae0bf271
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-11-21 23:16:50 +0000

    Remove old enums code

--- components/settings/enums/addEdit/addEditEnumConn.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import { SERVER_URL } from '../../../routing/addressAPI';
-
-export const addEditEnumConn = async (data: {}) => {
-    return await axios.put(`${SERVER_URL}/enums`, data, { headers: { Authorisation: 'Bearer ' + localStorage.getItem('token') } });
-};
