9ff27cf138b3c7d253b22e440c78d525355d8d49
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-12-01 20:47:08 +0000

    Settings access permissions

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -9,17 +9,25 @@ const Settings = () => {
 
     return (
         <div className="h-screen flex flex-row flex-wrap">
-            <IndexCardLayout label="Users">
-                <Link href="/settings/users" className="ml-8 hover:text-accent flex flex-row items-center">
-                    Users
-                </Link>
-                <Link href="/settings/userGroups" className="ml-8 hover:text-accent flex flex-row items-center">
-                    User Groups
-                </Link>
-                <Link href="/settings/permissions" className="ml-8 hover:text-accent flex flex-row items-center">
-                    User Group Permissions
-                </Link>
-            </IndexCardLayout>
+            {permissions.users?.view || permissions.userGroups?.view || isAdmin ? (
+                <IndexCardLayout label="Users">
+                    {permissions.users?.view || isAdmin ? (
+                        <Link href="/settings/users" className="ml-8 hover:text-accent flex flex-row items-center">
+                            Users
+                        </Link>
+                    ) : null}
+                    {permissions.userGroups?.view || isAdmin ? (
+                        <>
+                            <Link href="/settings/userGroups" className="ml-8 hover:text-accent flex flex-row items-center">
+                                User Groups
+                            </Link>
+                            <Link href="/settings/permissions" className="ml-8 hover:text-accent flex flex-row items-center">
+                                User Group Permissions
+                            </Link>
+                        </>
+                    ) : null}
+                </IndexCardLayout>
+            ) : null}
             {permissions.enums?.view || isAdmin ? (
                 <IndexCardLayout label="Enums">
                     <Link href="/settings/enumgroups" className="ml-8 hover:text-accent flex flex-row items-center">
