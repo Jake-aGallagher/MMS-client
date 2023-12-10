e85a75adf5ce8c2c31824b976b502e3df6c85193
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-12-10 20:11:37 +0000

    Redirect no longer necessary

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/jobs',
-        permanent: false
-      }
-    ]
-  }
 }
