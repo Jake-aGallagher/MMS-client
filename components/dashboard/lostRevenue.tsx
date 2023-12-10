6ed72d7fab109f06b3857df54088bfac7ee862a3
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-12-10 21:09:34 +0000

    Faux dashboard loading visualisation

--- components/dashboard/lostRevenue.tsx
+++ components/dashboard/lostRevenue.tsx
@@ -1,9 +1,15 @@
+import { useEffect, useState } from 'react';
 import DashChartComponent from './dashChartComponent';
 import { formatNumeric } from './formatNumeric';
+import Loading from '../loading/loading';
 
 const LostRevenue = () => {
     const lostRevenue = 58682;
     const avgData = { value: -4, flipped: false };
+
+    const [loading, setLoading] = useState(true);
+
+    setTimeout(() => setLoading(false), 2000);
 
     const mainData = [
         { label: 'June', value: 70000 },
@@ -25,7 +31,17 @@ const LostRevenue = () => {
     text.totalString = numText;
     text.afterTotalString = afterText;
 
-    return <DashChartComponent avgData={avgData} mainData={mainData} text={text} />;
+    return (
+        <>
+            {loading ? (
+                <div className="w-full h-full bg-secondary rounded-md relative">
+                    <Loading />
+                </div>
+            ) : (
+                <DashChartComponent avgData={avgData} mainData={mainData} text={text} />
+            )}
+        </>
+    );
 };
 
 export default LostRevenue;
