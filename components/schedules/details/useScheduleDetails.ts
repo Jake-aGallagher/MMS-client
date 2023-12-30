9e202f8d5fa74709d6b35225c71030d4a97729f6
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-12-30 15:35:32 +0000

    Schedules details

---
+++ components/schedules/details/useScheduleDetails.ts
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+import { SERVER_URL } from '../../routing/addressAPI';
+import axios from 'axios';
+
+interface Schedule {
+    id: number;
+    asset: string;
+    type: string;
+    title: string;
+    description: string;
+    frequency: string;
+    last_comp_date: string;
+    next_due_date: string;
+    up_to_date: number;
+}
+
+interface SchedulePMs {
+    id: number;
+    notes: string;
+    created: string;
+    required_comp_date: string;
+    completed: number;
+    comp_date: string;
+    logged_time: number;
+    status: string;
+}
+
+export const useScheduleDetails = (propertyId: number, scheduleId: string) => {
+    const [loading, setLoading] = useState(true);
+    const [noData, setNoData] = useState(false);
+    const [error, setError] = useState(false);
+    const [scheduleDetails, setScheduleDetails] = useState<Schedule>();
+    const [schedulePMs, setSchedulePMs] = useState<SchedulePMs[]>([]);
+
+    useEffect(() => {
+        reload();
+    }, []);
+
+    const reload = () => {
+        setLoading(true);
+        setError(false);
+        setNoData(false);
+        getScheduleHandler();
+    };
+
+    const getScheduleHandler = async () => {
+        try {
+            const response = await axios.get(`${SERVER_URL}/schedule-templates/${propertyId}/${scheduleId}`, {
+                headers: { Authorisation: 'Bearer ' + localStorage.getItem('token') },
+            });
+            if (response.data.scheduleDetails.length === 0) {
+                setNoData(true);
+            } else {
+                setScheduleDetails(response.data.scheduleDetails[0]);
+                setSchedulePMs(response.data.scheduleJobs);
+            }
+            setLoading(false);
+        } catch (err) {
+            setError(true);
+            setLoading(false);
+        }
+    };
+    return { scheduleDetails, schedulePMs, loading, noData, error, reload };
+};
---
+++ pages/schedules/[scheduleId].tsx
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import ModalBase from '../../components/modal/modal';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faPencil } from '@fortawesome/free-solid-svg-icons';
+import LoadingNoDataError from '../../components/loading/loadingNoDataError';
+import FullPage from '../../components/page/fullPage';
+import Toolbar from '../../components/page/toolbar';
+import DetailsBox from '../../components/detailsBox/detailsBox';
+import AttachedFilesBox from '../../components/attachedFilesBox/attachedFilesBox';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../components/store/store';
+import { useScheduleDetails } from '../../components/schedules/details/useScheduleDetails';
+import DataTable from '../../components/dataTable/dataTable';
+
+const ScheduleView = () => {
+    const permissions = useSelector((state: RootState) => state.permissions.value.permissions);
+    const isAdmin = useSelector((state: RootState) => state.user.value.isAdmin);
+    const router = useRouter();
+    if (!permissions.schedules?.view && !isAdmin) {
+        router.push('/');
+    }
+    const currentProperty = useSelector((state: RootState) => state.currentProperty.value.currentProperty);
+    const scheduleTemplateId = router.asPath.split('/')[2];
+    const { scheduleDetails, schedulePMs, loading, noData, error, reload } = useScheduleDetails(currentProperty, scheduleTemplateId);
+    const [viewModal, setViewModal] = useState(false);
+    const [modalType, setModalType] = useState('');
+
+    const scheduleDetailsConfig = {
+        id: scheduleDetails?.id,
+        fields: [
+            { label: 'ID Number', value: scheduleDetails?.id },
+            { label: 'Title', value: scheduleDetails?.title },
+            { label: 'Up to Date', value: scheduleDetails?.up_to_date == 1 ? <div>&#10004;</div> : <div>&#10060;</div> },
+            { label: 'Asset Name', value: scheduleDetails?.asset },
+            { label: 'Type', value: scheduleDetails?.type },
+            { label: 'Description', value: scheduleDetails?.description },
+            { label: 'Last Completed', value: scheduleDetails?.last_comp_date },
+            { label: 'Next Due', value: scheduleDetails?.next_due_date },
+            { label: 'Frequency', value: scheduleDetails?.frequency },
+        ],
+    };
+
+    const scheduleJobsTableConfig = {
+        headers: [
+            { id: 'id', name: 'PM ID', type: 'link', search: true, order: true },
+            { id: 'created', name: 'Date Created', type: 'string', search: true, order: true },
+            { id: 'required_comp_date', name: 'Due Date', type: 'string', search: true, order: true },
+            { id: 'completed', name: 'Completed', type: 'tick', search: true, order: true },
+            { id: 'comp_date', name: 'completion Date', type: 'string', search: true, order: true },
+            { id: 'logged_time', name: 'Total loged Time', type: 'number', search: true, order: true },
+            { id: 'status', name: 'Current Status', type: 'string', search: true, order: true },
+        ],
+        searchable: true,
+        reverseSort: true,
+        linkColPrefix: `/schedules/pm/`,
+    };
+
+    return (
+        <>
+            <FullPage>
+                <Toolbar>
+                    <Link href="/schedules" className="tLink">
+                        <FontAwesomeIcon icon={faArrowLeft} className="mr-1 w-3" />
+                        <p>Return to PM Schedules</p>
+                    </Link>
+                    {permissions.schedules?.manage || isAdmin ? (
+                        <button onClick={() => [setViewModal(true), setModalType('updateSchedule')]} className="tLink">
+                            <FontAwesomeIcon icon={faPencil} className="mr-1 w-3" />
+                            Update
+                        </button>
+                    ) : null}
+                </Toolbar>
+
+                <LoadingNoDataError loading={loading} error={error} noData={noData}>
+                    <>
+                        {viewModal ? <ModalBase modalType={modalType} payload={scheduleDetails?.id} closeModal={() => [setViewModal(false), reload()]} /> : ''}
+
+                        <div className="w-full h-full pt-4 flex flex-col">
+                            <div className="flex flex-col xl:flex-row">
+                                <DetailsBox data={scheduleDetailsConfig} />
+                                <div className="flex flex-col w-full">
+                                    <div className="w-full xl:pl-8">
+                                        <AttachedFilesBox model="scheduleTemplate" id={parseInt(scheduleTemplateId)} />
+                                    </div>
+                                </div>
+                            </div>
+                            {schedulePMs.length > 0 ? <DataTable config={scheduleJobsTableConfig} data={schedulePMs} /> : null}
+                        </div>
+                    </>
+                </LoadingNoDataError>
+            </FullPage>
+        </>
+    );
+};
+
+export default ScheduleView;
