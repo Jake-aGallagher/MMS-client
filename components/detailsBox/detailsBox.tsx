7bca65819e01d80a6c19177b508ad42799269595
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-12-30 17:00:47 +0000

    Allow label in details box

--- components/detailsBox/detailsBox.tsx
+++ components/detailsBox/detailsBox.tsx
@@ -1,6 +1,7 @@
 interface Props {
     data: {
         id?: number;
+        title?: string;
         fields: {
             label: string;
             value?: any;
@@ -11,6 +12,7 @@ interface Props {
 const DetailsBox = (props: Props) => {
     return (
         <div className=" mb-4 w-full max-w-3xl bg-secondary mt-2 p-2 rounded-xl shadow-xl">
+            {props.data.title ? <div className="text-xl text-center mb-4">{props.data.title}</div> : null}
             {props.data.fields.map((item) => (
                 <div key={item.label} className="flex flex-row mb-3 pb-1 border-b-1 border-text border-opacity-10">
                     <div className="w-1/4 pl-1">{item.label}: </div>
