9f5e390344599c27266e9f67b9528ead4cee1cac
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-02-11 17:43:47 +0000

    Red x if close modal passed in to form

--- components/forms/formContainer.tsx
+++ components/forms/formContainer.tsx
@@ -1,7 +1,26 @@
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { PropsWithChildren } from 'react';
 
-const FormContainer = (props: PropsWithChildren) => {
-    return <div className="h-full w-full rounded-md relative">{props.children}</div>;
+interface Props extends PropsWithChildren {
+    closeModal?: () => void;
+}
+
+const FormContainer = (props: Props) => {
+    return (
+        <div className="h-full w-full rounded-md relative">
+            {props.closeModal && (
+                <button
+                    className="group absolute w-6 h-6 border-solid border-1 border-red hover:bg-red right-2 top-2 rounded-md flex flex-row justify-center items-center transition-all"
+                    onClick={(e) => [e.preventDefault(), props.closeModal ? props.closeModal() : null]}
+                >
+                    <FontAwesomeIcon icon={faTimes} className="text-red w-4 group-hover:text-background transition-all" />
+                </button>
+            )}
+
+            {props.children}
+        </div>
+    );
 };
 
 export default FormContainer;
