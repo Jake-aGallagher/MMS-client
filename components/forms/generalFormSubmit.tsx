750b1b29a429d5678151ceb4508530de92cd7f97
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-02-14 21:07:43 +0000

    Allow for canel button only

--- components/forms/generalFormSubmit.tsx
+++ components/forms/generalFormSubmit.tsx
@@ -1,19 +1,21 @@
 interface Props {
     closeModal: () => void;
     submitLabel?: string;
+    cancelOnly?: boolean;
 }
 
 const GeneralFormSubmit = (props: Props) => {
     return (
         <div className="flex flex-row justify-end items-center absolute bottom-0 h-16 left-0 w-full">
-            <button className="btnBlue h-8 px-4 w-32" onClick={(e) => [e.preventDefault(), props.closeModal()]}>
+            <button className={`btnBlue h-8 px-4 w-32 ${props.cancelOnly && 'mr-4'}`} onClick={(e) => [e.preventDefault(), props.closeModal()]}>
                 Cancel
             </button>
-            {props.submitLabel == 'Delete' ? (
-                <button className="btnRed mx-4 h-8 px-4 w-32">{props.submitLabel}</button>
-            ) : (
-                <button className="btnBlue mx-4 h-8 px-4 w-32">{props.submitLabel ? props.submitLabel : 'Submit'}</button>
-            )}
+            {!props.cancelOnly &&
+                (props.submitLabel == 'Delete' ? (
+                    <button className="btnRed mx-4 h-8 px-4 w-32">{props.submitLabel}</button>
+                ) : (
+                    <button className="btnBlue mx-4 h-8 px-4 w-32">{props.submitLabel ? props.submitLabel : 'Submit'}</button>
+                ))}
         </div>
     );
 };
