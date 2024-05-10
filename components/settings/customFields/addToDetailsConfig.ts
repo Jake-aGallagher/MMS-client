e101a4fce9bbe2d0823ea2c44841360597603794
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-05-10 21:55:16 +0100

    Pass client id to pretty values component

--- components/settings/customFields/addToDetailsConfig.ts
+++ components/settings/customFields/addToDetailsConfig.ts
@@ -2,10 +2,10 @@ import { CustomFieldData } from '../../../commonTypes/CustomFields';
 import { DetailsConfig } from '../../../commonTypes/DetailsConfig';
 import { prettyFieldValues } from './fieldPrettyValues';
 
-export const addToDetailsConfig = (config: DetailsConfig, customFields: CustomFieldData) => {
+export const addToDetailsConfig = (clientId: string, config: DetailsConfig, customFields: CustomFieldData) => {
     const newConfig = { ...config };
     customFields.fields.forEach((field) => {
-        newConfig.fields.push({ label: field.name, value: prettyFieldValues(field.id, field.type, field.value || '', customFields.enumGroups, customFields.fileData, field.enumGroupId) });
+        newConfig.fields.push({ label: field.name, value: prettyFieldValues(clientId, field.id, field.type, field.value || '', customFields.enumGroups, customFields.fileData, field.enumGroupId) });
     });
     return newConfig;
 };
