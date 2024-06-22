fafe5b192eb158c046edec207070612e25d980e2
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-06-22 11:36:40 +0100

    Opacity

--- components/supplyChain/index/transportCard.tsx
+++ components/supplyChain/index/transportCard.tsx
@@ -6,7 +6,7 @@ const TransportCard = () => {
             <div className="bg-background p-6 rounded-md shadow-md">
                 <h2 className="text-xl font-semibold mb-2">Transport & Logistics (comming soon)</h2>
                 <p className="">Manage Transport and Logistics</p>
-                <button className="btnBlue opacity-70 hover:bg-background hover:text-black h-8 px-4 mt-4" disabled={true}>
+                <button className="btnBlue opacity-40 hover:bg-background hover:text-black h-8 px-4 mt-4" disabled={true}>
                     {/* <Link href="/supply-chain/transport">Manage Transport & Logistics</Link> */}
                     Manage Transport & Logistics
                 </button>
