022fc9a330edda13254a25038e5d37f2a7ba1a87
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-06-22 11:34:06 +0100

    Disable the transport card

--- components/supplyChain/index/transportCard.tsx
+++ components/supplyChain/index/transportCard.tsx
@@ -1,13 +1,14 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const TransportCard = () => {
     return (
         <div className="px-10 pt-5">
             <div className="bg-background p-6 rounded-md shadow-md">
-                <h2 className="text-xl font-semibold mb-2">Transport & Logistics</h2>
+                <h2 className="text-xl font-semibold mb-2">Transport & Logistics (comming soon)</h2>
                 <p className="">Manage Transport and Logistics</p>
-                <button className="btnBlue h-8 px-4 mt-4">
-                    <Link href="/supply-chain/transport">Manage Transport & Logistics</Link>
+                <button className="btnBlue opacity-70 hover:bg-background hover:text-black h-8 px-4 mt-4" disabled={true}>
+                    {/* <Link href="/supply-chain/transport">Manage Transport & Logistics</Link> */}
+                    Manage Transport & Logistics
                 </button>
             </div>
         </div>
